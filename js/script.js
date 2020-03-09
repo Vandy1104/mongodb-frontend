938250f2a5cb4fc5cbf4339bb87de675ce97bb6c
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-03-09 01:47:07 +0000

    login & session storage working

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 console.log("front-end");
+console.log(sessionStorage);
+let url;
 
 $(document).ready(function(){
   $('#heading').click(function(){
@@ -9,11 +11,35 @@ $(document).ready(function(){
   $('#adminBtn').click(function(){
     $('#adminPage').show();
     $('#homePage').hide();
-  })
+  });
+  $('#homeBtn').click(function(){
+    $('#adminPage').hide();
+    $('#homePage').show();
+  });
+
+//get url and port from config.json
+  $.ajax({
+    url :'config.json',
+    type :'GET',
+    dataType :'json',
+    success : function(configData){
+      console.log(configData);
+      url = `${configData.SERVER_URL}:${configData.SERVER_PORT}`;
+      console.log(url);
+
+    },//success
+    error:function(){
+      console.log('error: cannot call api');
+    }//error
+
+
+  });//ajax
+
+
 
   $('#viewUserBtn').click(function(){
     $.ajax({
-      url :'192.168.33.10:3000/allUsers',
+      url :`${url}/allUsers`,
       type :'GET',
       dataType :'json',
       success : function(usersFromMongo){
@@ -24,7 +50,68 @@ $(document).ready(function(){
       }//error
 
 
-    });//ajax
-  });//document.ready
-
+    });//View User button
 });
+
+    $('#viewProductsBtn').click(function(){
+      $.ajax({
+        url :`${url}/showProducts`,
+        type :'GET',
+        dataType :'json',
+        success : function(productsFromMongo){
+          console.log(productsFromMongo);
+          document.getElementById('productCards').innerHTML = "";
+
+      for(let i=0; i<productsFromMongo.length; i++){
+        document.getElementById('productCards').innerHTML +=
+        `<div class="col">
+        <h3 class=""> ${productsFromMongo[i].name}</h3>
+        <h4 class="">$${productsFromMongo[i].price}</h4>
+        </div>`;
+
+      }
+
+
+    },//success
+        error:function(){
+          console.log('error: cannot call api');
+        }//error
+
+      }); //Ajax
+      });//View Products Button
+
+
+    $('#loginForm').submit(function(){
+      event.preventDefault();
+      let username = $('#username').val();
+      let password = $('#password').val();
+      console.log(username,password);
+      $.ajax({
+        url :`${url}/loginUser`,
+        type :'POST',
+        data:{
+          username : username,
+          password : password
+          },
+        success : function(loginData){
+          console.log(loginData);
+          if(loginData === 'user not found. Please register') {
+            alert('Register Please');
+          } else {
+            sessionStorage.setItem('userId', loginData['_id']);
+            sessionStorage.setItem('userName', loginData['username']);
+            sessionStorage.setItem('userEmail', loginData['email']);
+            console.log(sessionStorage);
+          }
+          //console.log('you are logged in');
+        },//success
+        error:function(){
+          console.log('error: cannot call api');
+        }//error
+
+
+      });//ajax
+
+    });//submit function
+
+  });//document.ready
